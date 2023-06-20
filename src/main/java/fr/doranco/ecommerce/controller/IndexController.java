0021b830290d0435dc3ff549514d8365c6274816
Hugo <evilzorua@live.com>
2023-06-20 11:36:55 +0200

    connection bdd

--- src/main/java/fr/doranco/ecommerce/controller/IndexController.java
+++ src/main/java/fr/doranco/ecommerce/controller/IndexController.java
@@ -2,12 +2,13 @@ package fr.doranco.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class IndexController {
-    @RequestMapping("/test")
+    @RequestMapping(value = "/test")
     public String index(Model model) {
         model.addAttribute("test", "test");
         return "index";
