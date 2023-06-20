1036eed0bd4448b2afd5c9d7be403270fd0b647f
Kevin B <kevin.b@ikmail.com>
2023-06-20 11:46:29 +0200

    Param

--- src/main/java/fr/doranco/ecommerce/controller/IndexController.java
+++ src/main/java/fr/doranco/ecommerce/controller/IndexController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/test")
 public class IndexController {
 
-	@RequestMapping("/")
-	public String index(Model model) {
-		model.addAttribute("test", "test");
+	@RequestMapping("")
+	public String index(@RequestParam(name = "name", required = false, defaultValue = "test") String name,
+			Model model) {
+		model.addAttribute("name", name);
 		return "index";
 	}
 }
